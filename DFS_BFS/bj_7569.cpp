c147e317113667970c13646eea0674c1c1d33c27
SongmiLim <99317323+SongmiLim@users.noreply.github.com>
2024-01-18 09:46:55 +0000

    ps

---
+++ DFS_BFS/bj_7569.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <queue>
+#define MAX 100
+
+int M, N, H;
+int map[MAX][MAX][MAX];
+int visited[MAX][MAX][MAX];
+int dx[6] = {0,0,-1,1,0,0};
+int dy[6] = {0,0,0,0,1,-1};
+int dz[6] = {1,-1,0,0,0,0};
+
+void bfs() {
+  std::queue <std::pair<std::pair<int, int>, int>>  q;
+
+  for (int k = 0; k < H; k++) {
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < M; j++) {
+        if (map[i][j][k] == 1) {
+          q.push({ { i, j }, k });
+          visited[i][j][k] = 1;
+        }
+      }
+    }
+  }
+
+  while (!q.empty()) {
+    int x = q.front().first.first;
+    int y = q.front().first.second;
+    int z = q.front().second;
+    q.pop();
+
+    for (int i = 0; i < 6; i++) {
+      int nx = x + dx[i];
+      int ny = y + dy[i];
+      int nz = z + dz[i];
+
+      if (nx < 0 || ny < 0 || nz < 0 || nx >= N || ny >= M || nz >= H)
+        continue;
+
+      if (visited[nx][ny][nz] == 0 && map[nx][ny][nz] != -1) {
+        visited[nx][ny][nz] = visited[x][y][z] + 1;
+        map[nx][ny][nz] = 1;
+        q.push({ {nx, ny}, nz });
+      }
+    }
+  }
+}
+
+int result() {
+  int max = 0;
+
+  for (int k = 0; k < H; k++) {
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < M; j++) {
+        if (map[i][j][k] == 0)
+          return -1;
+
+        if (visited[i][j][k] > max)
+          max = visited[i][j][k];
+      }
+    }
+  }
+  return max - 1;
+}
+
+int main() {
+  std::cin >> M >> N >> H;
+
+  for (int k = 0; k < H; k++) {
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < M; j++) {
+        std::cin >> map[i][j][k];
+      }
+    }
+  }
+
+  bfs();
+  std::cout << result();
+
+  return 0;
+}
---
+++ DP/bj_12865.cpp
@@ -0,0 +1,58 @@
+////////// 2차원 배열 이용
+#include <iostream>
+#include <string.h>
+#include <algorithm>
+
+int N, K;
+int weight[101];
+int value[101];
+int dp[101][100001];
+
+int main() {
+  std::cin >> N >> K;
+
+  for (int i = 1; i <= N; i++) {
+    std::cin >> weight[i] >> value[i];
+  }
+
+  for (int i = 1; i <= N; i++) {
+    for (int w = 1; w <= K; w++) {
+      if (weight[i] <= w)   
+        // max(이전 아이템의 최대 가치, 이전 아이템 빼고 현재 물건을 넣는 가치)
+        dp[i][w] = std::max(dp[i - 1][w], value[i] + dp[i - 1][w - weight[i]]);  
+      else 
+        dp[i][w] = dp[i-1][w];
+    }
+  }
+  std::cout << dp[N][K];
+
+  return 0;
+}
+
+///////// 1차원 배열 이용
+/*#include <iostream>
+#include <string.h>
+#include <algorithm>
+
+int N, K;
+int weight[101];
+int value[101];
+int dp[100001];
+
+int main() {
+  std::cin >> N >> K;
+
+  for (int i = 1; i <= N; i++) {
+    std::cin >> weight[i] >> value[i];
+  }
+
+  for (int i = 1; i <= N; i++) {
+    for (int j = K; j >= 1; j--) {
+      if (j - weight[i] >= 0)
+        dp[j] = std::max(dp[j], value[i] + dp[j - weight[i]]);
+    }
+  }
+  std::cout << dp[K];
+
+  return 0;
+}*/
