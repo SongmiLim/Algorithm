f63c16e323e80dfe7254e2e20ca186a9f71822de
SongmiLim <99317323+SongmiLim@users.noreply.github.com>
2024-03-09 19:20:29 +0900

    [Silver I] Title: 단지번호붙이기, Time: 12 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+#define MAX 25
+int N;
+int map[MAX][MAX];
+bool visit[MAX][MAX];
+int cnt;
+std::vector<int> count_list;
+
+int dx[4] = { 0, 0, -1, 1 };
+int dy[4] = { 1, -1, 0, 0 };
+
+void bfs(int x, int y) {
+	std::queue<std::pair<int, int>> q;
+	cnt = 1;
+	q.push({ x, y });
+	visit[x][y] = true;
+	
+	while (!q.empty()) {
+		int x = q.front().first;
+		int y = q.front().second;
+
+		q.pop();
+
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+
+			if (nx < 0 || ny < 0 || nx >= N || ny >= N) {
+				continue;
+			}
+			if (!visit[nx][ny] && map[nx][ny] == 1) {
+				visit[nx][ny] = true;
+				q.push({ nx, ny });
+				cnt++;
+			}
+		}
+	}
+	count_list.push_back(cnt);
+}
+
+int main() {
+
+	std::cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		std::string str;
+		std::cin >> str;
+		for (int j = 0; j < N; j++) {
+			map[i][j] = str[j] - '0';  // str => int형으로 변환
+		}
+	}
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (!visit[i][j] && map[i][j] == 1) {
+				bfs(i, j);
+			}
+		}
+	}
+	std::sort(count_list.begin(), count_list.end());
+	std::cout << count_list.size() << std::endl;
+
+	for (int i = 0; i < count_list.size(); i++) {
+		std::cout << count_list[i] << std::endl;
+	}
+}
