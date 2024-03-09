21dee6a0c377b1d025dd71d329f0ba1f80f9af83
SongmiLim <99317323+SongmiLim@users.noreply.github.com>
2024-03-09 21:18:43 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 4.01 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cpp
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cpp
@@ -7,17 +7,17 @@ using namespace std;
 
 int solution(int n) {
     int answer = 0;
-    int k = 3;
+    int k = 3;   // k에 할당하는 수에따라 2진법,3진법,,, 등 적용 가능하다
     vector <int> v;
-    
-   while (true) {
-       v.push_back(n%k);
-       n = n/k;
-       if(n==0)
-           break;
-   }
-    for(int i=0;i<v.size();i++){
-        answer += v[i]*pow(3,v.size()-i-1);
+
+    while (true) {
+        v.push_back(n % k);
+        n = n / k;
+        if (n == 0)
+            break;
+    }
+    for (int i = 0; i < v.size(); i++) {
+        answer += v[i] * pow(3, v.size() - i - 1);
     }
     return answer;
 }
