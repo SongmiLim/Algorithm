6122261d703022ec1de73df794025aec7d1273c6
SongmiLim <99317323+SongmiLim@users.noreply.github.com>
2024-03-09 19:28:06 +0900

    [Bronze II] Title: 벌집, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+	int N;
+	int cnt = 0;
+	std::cin >> N;
+	
+	while (1) {
+		N -= cnt * 6;
+		cnt++;
+
+		if (N <=1)
+			break;
+	}
+	std::cout << cnt;
+	return 0;
+}
