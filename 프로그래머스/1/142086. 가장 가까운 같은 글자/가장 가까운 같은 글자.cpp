9531dc2d34f0f622c4848fe132b5305173f53b23
SongmiLim <99317323+SongmiLim@users.noreply.github.com>
2024-03-10 15:28:44 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 2.69 ms, Memory: 5.25 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.cpp
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.cpp
@@ -1,8 +1,29 @@
 #include <string>
 #include <vector>
+#include <map>
 
 using namespace std;
 
+vector<int> solution(string s)
+{
+    map<char, int> mp;
+    vector<int> answer;
+
+
+    for (int i = 0; i < s.size(); ++i)
+    {
+        if (mp.find(s[i]) != mp.end()) 
+            answer.push_back(i - mp[s[i]]);
+        else
+            answer.push_back(-1);
+        // map 에 key, value 넣기   => 왜 여기서  => insert 시 중복된 키값이 들어오면 insert 불가
+        // 따라서 해당 키에 따른 value값이 업데이트 되면 직접 값을 업데이트 시켜줘야함
+        mp[s[i]] = i;  
+    }
+    return answer;
+}
+
+/*
 vector<int> solution(string s) {
     vector<int> answer;
     
@@ -18,3 +39,4 @@ vector<int> solution(string s) {
     }
     return answer;
 }
+*/
