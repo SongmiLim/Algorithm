631dc06502aa706866d162db83e797a917c6a346
SongmiLim <99317323+SongmiLim@users.noreply.github.com>
2024-03-10 16:05:06 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.04 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
@@ -1,67 +1,22 @@
 #include <string>
 #include <vector>
-
+#include <regex>
 using namespace std;
 
 int solution(string s) {
     int answer = 0;
-    string ans;
-    int cnt = 0;
-    string tmp;
-    
-    for (int i = 0; i < s.size(); i++) {
-        if ('0' <= s[i] && s[i] <= '9') {
-            ans += s[i];
-            continue;
-        }
-        else {
-            cnt++;
-            tmp += s[i];
-        }
 
-        if (cnt > 0) {
-            if (tmp == "zero") {
-                ans += '0';
-                tmp.clear();
-            }
-            else if (tmp == "one") {
-                ans += '1';
-                tmp.clear();
-            }
-            else if (tmp == "two") {
-                ans += '2';
-                tmp.clear();
-            }
-            else if (tmp == "three") {
-                ans += '3';
-                tmp.clear();
-            }
-            else if (tmp == "four") {
-                ans += '4';
-                tmp.clear();
-            }
-            else if (tmp == "five") {
-                ans += '5';
-                tmp.clear();
-            }
-            else if (tmp == "six") {
-                ans += '6';
-                tmp.clear();
-            }
-            else if (tmp == "seven") {
-                ans += '7';
-                tmp.clear();
-            }
-            else if (tmp == "eight") {
-                ans += '8';
-                tmp.clear();
-            }
-            else if (tmp == "nine") {
-                ans += '9';
-                tmp.clear();
-            }
-        }
-    }
-    answer = stoi(ans);
+    s = regex_replace(s, regex("zero"), "0");
+    s = regex_replace(s, regex("one"), "1");
+    s = regex_replace(s, regex("two"), "2");
+    s = regex_replace(s, regex("three"), "3");
+    s = regex_replace(s, regex("four"), "4");
+    s = regex_replace(s, regex("five"), "5");
+    s = regex_replace(s, regex("six"), "6");
+    s = regex_replace(s, regex("seven"), "7");
+    s = regex_replace(s, regex("eight"), "8");
+    s = regex_replace(s, regex("nine"), "9");
+    
+    answer = stoi(s);
     return answer;
 }
