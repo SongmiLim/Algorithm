a81a9a545e6151bf2d2885919fec496f6030a444
SongmiLim <99317323+SongmiLim@users.noreply.github.com>
2024-03-11 14:22:16 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
@@ -4,13 +4,27 @@
 using namespace std;
 
 string solution(vector<int> food) {
-    string answer = "0";
-    for (int i = food.size() - 1; i > 0; i--) {
-        int cnt = food[i]/2;
-        while (cnt) {
-            answer = to_string(i) + answer + to_string(i);
-            cnt--;
+    string answer = "";
+    int total_num = 1;
+    for (int i = 1; i < food.size(); i++) {
+        if (food[i] % 2 != 0) {
+            food[i] -= 1;
         }
+        total_num += food[i];
+    }
+
+    vector <int> food_list(total_num,0);
+    int pos = 0;
+
+    for (int i = 1; i < food.size(); i++) {
+        for (int j = 0; j < food[i] / 2; j++) {
+            food_list[pos] = i;
+            food_list[total_num - 1 - pos] = i;
+            pos++;
+        }
+    }
+    for (auto& a : food_list) {
+        answer += to_string(a) ;
     }
 
     return answer;
