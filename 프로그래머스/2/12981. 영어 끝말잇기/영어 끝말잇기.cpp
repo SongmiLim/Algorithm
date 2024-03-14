d696838dd43803b47bbd42e3b9c1ce37cf76f3a3
SongmiLim <99317323+SongmiLim@users.noreply.github.com>
2024-03-14 17:34:49 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
@@ -12,7 +12,7 @@ vector<int> solution(int n, vector<string> words) {
     m[words[0]] = 1;
 
     for (int i = 1; i < words.size(); i++) {
-        if (m[words[i]] == 1 || words[i - 1][words[i - 1].size() - 1] != words[i][0]) {
+        if (m.find(words[i])!=m.end() || words[i - 1][words[i - 1].size() - 1] != words[i][0]) {
             answer.push_back(i % n + 1);
             answer.push_back(i / n + 1);
             break;
