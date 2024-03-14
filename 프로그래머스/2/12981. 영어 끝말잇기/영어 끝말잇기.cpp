2c5dbb98087a893fed5b3dda8618e936cc4d1f82
SongmiLim <99317323+SongmiLim@users.noreply.github.com>
2024-03-14 17:29:05 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.07 ms, Memory: 4.11 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
@@ -2,33 +2,26 @@
 #include <vector>
 #include <iostream>
 #include <cmath>
+#include <map>
+
 using namespace std;
 
 vector<int> solution(int n, vector<string> words) {
     vector<int> answer;
-    vector<string> tmp;
-    bool flag = false;
+    map<string, int> m;
+    m[words[0]] = 1;
 
     for (int i = 1; i < words.size(); i++) {
-        if (flag)
-            break;
-
-        tmp.push_back(words[i - 1]);
-        if (words[i - 1][words[i - 1].size() - 1] != words[i][0]) {
-            answer.push_back((i + 1) % n == 0 ? n : (i + 1) % n);
-            answer.push_back((i + 1) % n == 0 ? (i + 1) / n : (i + 1) / n + 1);
+        if (m[words[i]] == 1 || words[i - 1][words[i - 1].size() - 1] != words[i][0]) {
+            answer.push_back(i % n + 1);
+            answer.push_back(i / n + 1);
             break;
         }
-
-        for (int j = 0; j < tmp.size(); j++) {
-            if (tmp[j] == words[i]) {
-                answer.push_back((i + 1) % n == 0 ? n : (i + 1) % n);
-                answer.push_back((i + 1) % n == 0 ? (i + 1) / n : (i + 1) / n + 1);
-                flag = true;
-                break;
-            }
+        else {
+            m[words[i]] = 1;
         }
     }
+
     if (answer.size() == 0) {
         answer.push_back(0);
         answer.push_back(0);
