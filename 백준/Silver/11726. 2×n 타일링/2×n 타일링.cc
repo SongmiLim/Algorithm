2e05c1310c97c3d754491460f21009a6dfd69d60
Songmi Lim <99317323+SongmiLim@users.noreply.github.com>
2024-03-30 19:34:48 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int answer = 0;
+	int dp[1001] = { 0, };
+	int n;
+
+	cin >> n;
+	dp[1] = 1;
+	dp[2] = 2;
+
+	for (int i = 3; i <= n; i++) {
+		dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+	}
+
+	answer = dp[n];
+	cout << answer;
+	
+	return 0;
+}
