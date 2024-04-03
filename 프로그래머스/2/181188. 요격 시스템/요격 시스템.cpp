b1c076cdbbb73c8e5480ca3b6051ca52f34da7bc
Songmi Lim <99317323+SongmiLim@users.noreply.github.com>
2024-04-03 19:39:01 +0900

    [level 2] Title: 요격 시스템, Time: 0.03 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.cpp
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.cpp
@@ -4,10 +4,16 @@
 #include <algorithm>
 using namespace std;
 
+bool cmp(vector<int> &a, vector<int> &b){
+    if(a[0] == b[0]){
+        return a[1]<b[1];
+    }
+}
+
 int solution(vector<vector<int>> targets) {
     int answer = 0;
     
-    sort(targets.begin(), targets.end());
+    sort(targets.begin(), targets.end(), cmp);
     
     int end = 0;
     for(auto& target: targets){
@@ -15,9 +21,6 @@ int solution(vector<vector<int>> targets) {
             cout<< target[0]<<endl;
             end = target[1];
             answer++;
-        }
-        else{
-            end = min(end, target[1]);
         }
     }
     return answer;
