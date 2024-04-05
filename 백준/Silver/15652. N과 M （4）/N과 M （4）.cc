fa1dffeaa9e370a0cb31abf503e122c34665663e
Songmi Lim <99317323+SongmiLim@users.noreply.github.com>
2024-04-05 17:23:33 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -1,32 +1,30 @@
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
 
 int N, M;
-vector<int> num_list;
-bool visited[9];
+vector <int> num_list;
+vector <int> answer;
 
-void solve(int idx, int cnt) {
+void solution(int idx, int cnt) {
 	if (cnt == M) {
-		for (auto& a : num_list) {
+		for (auto& a : num_list)
 			cout << a << " ";
-		}
 		cout << '\n';
 		return;
 	}
 
-	for (int i = idx; i <= N; i++) {
-		if (!visited[i]) {
-			num_list.push_back(i);
-			solve(i, cnt + 1);
-			num_list.pop_back();
-		}
+	for (int i = idx; i <= N; ++i) {
+		num_list.push_back(i);
+		solution(i, cnt + 1);
+		num_list.pop_back();
 	}
 }
 
 int main() {
 	cin >> N >> M;
-	solve(1, 0);
+	solution(1, 0);
 
 	return 0;
 }
