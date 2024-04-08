69bd7705d7d7e9c600ef2b13d3b93bd96fb761a8
Songmi Lim <99317323+SongmiLim@users.noreply.github.com>
2024-04-08 15:36:16 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -1,24 +1,24 @@
 #include <string>
 #include <vector>
-#include <stack>
+#include <queue>
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
-    stack<int>st;
+    queue<int>q;
     int cnt;
-    for (int i = progresses.size() - 1; i >= 0; i--) {
-        st.push(progresses[i]);
+    for (int i = 0; i <progresses.size(); i++) {
+        q.push(progresses[i]);
     }
 
-    while (!st.empty()) {
+    while (!q.empty()) {
         cnt = 0;
         for (int i = 0; i < progresses.size(); i++) {
             progresses[i] += speeds[i];
         }
 
-        while (!st.empty() && progresses[progresses.size() - st.size()] >= 100) {
-            st.pop();
+        while (!q.empty() && progresses[progresses.size() - q.size()] >= 100) {
+            q.pop();
             cnt++;
         }
 
