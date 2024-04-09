d7d237aac1bd242f714b0876cb06ef01e99dde9c
Songmi Lim <99317323+SongmiLim@users.noreply.github.com>
2024-04-09 16:03:53 +0900

    [level 3] Title: 네트워크, Time: 0.04 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+#define MAX 200
+using namespace std;
+
+bool visited[MAX];
+void bfs(vector<vector<int>> computers, int computer_index) {
+    queue <int> q;
+    q.push(computer_index);
+    visited[computer_index] = true;
+    
+    while(!q.empty()) {
+        int index = q.front();
+        q.pop();
+        
+        for(int i=0;i<computers.size();i++) {
+            if(computers[index][i] == 1 && !visited[i]) {
+                visited[i] = true;
+                q.push(i);
+            }
+        }
+    }
+}
+    
+int solution(int n, vector<vector<int>> computers) {
+    int answer = 0;
+    for(int i=0;i<computers.size();i++) {
+        if(!visited[i]) {
+            bfs(computers, i);
+            answer++;
+        }
+    }
+    return answer;
+}
