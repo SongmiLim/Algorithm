bf78c7fcb9f5e329069f732a1f7b11860dec1e73
Songmi Lim <99317323+SongmiLim@users.noreply.github.com>
2024-04-10 17:34:35 +0900

    [level 1] Title: 모의고사, Time: 0.42 ms, Memory: 4.03 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+int get_score(vector<int> answers, vector<int> student_method) {
+    vector<int> student_ans;
+    int score = 0;
+    
+    for(int i=0;i<answers.size();i++) {
+        student_ans.push_back(student_method[i%student_method.size()]);
+    }
+    
+    for(int i=0;i<answers.size();i++) {
+        if(answers[i] == student_ans[i]) {
+            score++;
+        }
+    }
+    return score;
+}
+
+vector<int> get_max_score(vector<int> answers) {
+    vector<int> temp;
+    vector<int> max_score;
+    
+    temp.push_back(get_score(answers, {1,2,3,4,5}));
+    temp.push_back(get_score(answers, {2, 1, 2, 3, 2, 4, 2, 5}));
+    temp.push_back(get_score(answers, {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}));
+    
+    int max = -1;
+    for(int i=0;i<temp.size();i++) {
+        if(max == temp[i]){
+            max_score.push_back(i+1);
+        }
+        else if(max < temp[i]){
+            max = temp[i];
+            max_score.clear();
+            max_score.push_back(i+1);
+        }
+    }
+    return max_score;
+}
+
+vector<int> solution(vector<int> answers) {
+    vector<int> answer;
+    
+    answer = get_max_score(answers);
+    return answer;
+}
