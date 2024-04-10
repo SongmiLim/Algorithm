b0d4f4e7e17a547f328d8a5388ec6f74529ab4d4
Songmi Lim <99317323+SongmiLim@users.noreply.github.com>
2024-04-10 15:51:21 +0900

    [Silver II] Title: N과 M (9), Time: 12 ms, Memory: 2064 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -8,18 +8,20 @@ int m, n;
 vector <int> list;
 int visited[10001];
 vector <int> v;
-set <vector<int>> s;
-
 void solve(int idx, int cnt) {
 	if (cnt == m) {
-		s.insert(v);
+		for (auto& a : v) {
+			cout << a << " ";
+		}
+		cout << "\n";
 		return;
 	}
-
+	int tmp = 0;
 	for (int i = 0; i < list.size(); i++) {
-		if (!visited[i]) {
+		if (!visited[i]&&tmp!=list[i]) {
 			visited[i] = true;
 			v.push_back(list[i]);
+			tmp = list[i];
 			solve(i, cnt + 1);
 			visited[i] = false;
 			v.pop_back();
@@ -37,11 +39,5 @@ int main() {
 	sort(list.begin(), list.end());
 	solve(0, 0);
 
-	for (auto& a : s) {
-		for (int i = 0; i < a.size(); i++) {
-			cout << a[i] << " ";
-		}
-		cout << "\n";
-	}
 	return 0;
 }
