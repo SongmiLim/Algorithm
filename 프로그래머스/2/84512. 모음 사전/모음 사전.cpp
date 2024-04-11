a86b53b3474b8bafea423ba2a689d3efc60fc71b
Songmi Lim <99317323+SongmiLim@users.noreply.github.com>
2024-04-11 15:46:28 +0900

    [level 2] Title: 모음 사전, Time: 0.82 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
@@ -0,0 +1,57 @@
+#include <string>
+#include <vector>
+#include <cmath>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+vector <string> v;
+vector <vector <string>> index_words;
+
+void get_all_words(vector <string> all_words) {
+    
+    index_words.push_back(v);
+
+    if(v.size() == all_words.size()) {
+        return;
+    }
+    
+    for(int i=0;i<all_words.size();i++) {
+        v.push_back(all_words[i]);
+        get_all_words(all_words);
+        v.pop_back();
+    }
+}
+
+int get_word_index(string word) {
+    int cnt = 0;
+    
+    for(auto&a : index_words) {
+        string s = "";
+        for(int i=0;i<a.size();i++) {
+            s+=a[i];
+        }
+        
+        if(s == word){
+            return cnt;
+        }
+        cnt++;
+    } 
+}
+
+int solution(string word) {
+    int answer = 0;
+    get_all_words({"A", "E", "I", "O", "U"});
+    
+    for(auto&a : index_words) {
+        for(int i=0;i<a.size();i++) {
+            //cout<<a[i];
+        }
+        //cout<<"\n";
+    }
+    
+    answer = get_word_index(word);
+    
+    return answer;
+}
