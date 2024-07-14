52f1117f8efefbd0571a4e8207970afd158423fb
Songmi Lim <99317323+SongmiLim@users.noreply.github.com>
2024-07-14 18:49:42 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -1,69 +1,76 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <algorithm>
+#include <queue>
 
-#define MAX 25
-int N;
+#define MAX 26
+
+int n;
 int map[MAX][MAX];
-bool visit[MAX][MAX];
-int cnt;
-std::vector<int> count_list;
+bool visited[MAX][MAX];
 
+int total_home_num = 0;
+std::vector<int> home_cnt_list;
+
+int dy[4] = { -1, 1, 0, 0 };
 int dx[4] = { 0, 0, -1, 1 };
-int dy[4] = { 1, -1, 0, 0 };
 
-void bfs(int x, int y) {
+int get_home_cnt(int y, int x) {
+	int cnt = 0;
 	std::queue<std::pair<int, int>> q;
-	cnt = 1;
-	q.push({ x, y });
-	visit[x][y] = true;
-	
-	while (!q.empty()) {
-		int x = q.front().first;
-		int y = q.front().second;
+	q.push({ y, x });
+	visited[y][x] = true;
 
+	while (!q.empty()) {
+		cnt++;
+		int cy = q.front().first;
+		int cx = q.front().second;
 		q.pop();
 
 		for (int i = 0; i < 4; i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
+			int ny = cy + dy[i];
+			int nx = cx + dx[i];
 
-			if (nx < 0 || ny < 0 || nx >= N || ny >= N) {
+			if (ny < 0 || nx < 0 || ny >= n || nx >= n)
 				continue;
-			}
-			if (!visit[nx][ny] && map[nx][ny] == 1) {
-				visit[nx][ny] = true;
-				q.push({ nx, ny });
-				cnt++;
+
+			if (!visited[ny][nx] && map[ny][nx] == 1) {
+				q.push({ ny, nx });
+				visited[ny][nx] = true;
 			}
 		}
 	}
-	count_list.push_back(cnt);
+	return cnt;
 }
 
 int main() {
+	std::cin >> n;
 
-	std::cin >> N;
-
-	for (int i = 0; i < N; i++) {
-		std::string str;
-		std::cin >> str;
-		for (int j = 0; j < N; j++) {
-			map[i][j] = str[j] - '0';  // str => int형으로 변환
+	for (int i = 0; i < n; i++) {
+		std::string line;
+		std::cin >> line;
+		for (int j = 0; j < n; j++) {
+			map[i][j] = line[j] - '0';
+			visited[i][j] = false;
 		}
 	}
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			if (!visit[i][j] && map[i][j] == 1) {
-				bfs(i, j);
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (!visited[i][j] && map[i][j] == 1) {
+				total_home_num++;
+				int home_cnt = get_home_cnt(i, j);
+				home_cnt_list.push_back(home_cnt);
 			}
 		}
 	}
-	std::sort(count_list.begin(), count_list.end());
-	std::cout << count_list.size() << std::endl;
 
-	for (int i = 0; i < count_list.size(); i++) {
-		std::cout << count_list[i] << std::endl;
+	std::cout << total_home_num<<std::endl;
+
+	sort(home_cnt_list.begin(), home_cnt_list.end());
+	for (auto a : home_cnt_list) {
+		std::cout << a << "\n";
 	}
+
+	return 0;
 }
