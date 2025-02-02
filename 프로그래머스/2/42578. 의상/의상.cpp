21cf6a3e38420a507f8ec36c6c008c8bae106a88
Songmi Lim <99317323+SongmiLim@users.noreply.github.com>
2025-02-02 20:33:09 +0900

    [level 2] Title: 의상, Time: 0.03 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.cpp
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -1,25 +1,20 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
-#include <algorithm>
+
 using namespace std;
 
 int solution(vector<vector<string>> clothes) {
     int answer = 1;
-    vector <int> cnt_list;
-    unordered_map <string, vector<string>> vector_map;
+    unordered_map<string, int> category_num;
     
-    for(int i=0; i<clothes.size(); i++) {
-        vector_map[clothes[i][1]].push_back(clothes[i][0]);
+    for(auto& cloth: clothes) {
+        category_num[cloth[1]]++;
     }
     
-    for(auto a: vector_map) {
-        cnt_list.push_back(a.second.size() + 1);
+    for(auto& pair: category_num) {
+        answer *= pair.second + 1;
     }
-    
-    for(int i=0; i<cnt_list.size();i++) {
-        answer *= cnt_list[i];
-    }
-    answer--;
-    return answer;
+ 
+    return answer - 1;
 }
